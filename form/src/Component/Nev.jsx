28dce5a526ae4fe10cf0e568575507b2a612ddc2
DarshanPawar21 <dp7878038@gmail.com>
2026-03-25 13:53:45 +0530

    25/03/2026

--- form/src/Component/Nev.jsx
+++ form/src/Component/Nev.jsx
@@ -1,4 +1,6 @@
-
+import { Navigate } from "react-router"
+import { useNavigate } from "react-router"
+import w from "./Form.jsx"
 const Nev = () => {
     return (<>
         <div>
@@ -8,19 +10,19 @@ const Nev = () => {
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div className="collapse navbar-collapse" id="navbarNav">
-                        <ul className="navbar-nav">
+                    <div className="collapse navbar-collapse d-flex justify-content-center" id="navbarNav">
+                        <ul className="navbar-nav d-flex gap-5 me-5">
                             <li className="nav-item">
-                                <a className="nav-link active" aria-current="page" href="#">Home</a>
+                                <a className="nav-link" aria-current="page" href="/">Home</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="#">Features</a>
+                                <a className="nav-link" href="/user">User</a>
                             </li>
                             <li className="nav-item">
                                 <a className="nav-link" href="#">Pricing</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link disabled" aria-disabled="true">Disabled</a>
+                                <a className="nav-link" aria-disabled="true">Disabled</a>
                             </li>
                         </ul>
                     </div>
--- form/src/Component/user.jsx
+++ form/src/Component/user.jsx
@@ -2,16 +2,52 @@ import { useState } from "react";
 
 const User = () => {
     const [users, setuser] = useState(JSON.parse(localStorage.getItem("users"))) || [];
-    return (<div className="container d-flex gap-4 mt-4">
-        {
-            users.map((use) => (
-                <div>
-                    <h3>{use.name}</h3>
-                    <span className="d-flex flex-column">{use.email}</span>
-                    <span>{use.phone}</span>
-                </div>
-            ))
-        }
-    </div>)
+    const [search, setsearch] = useState("");
+    return (
+        <>
+            <h3 className="text-center">Customer Infomation</h3>
+            <hr></hr>
+            <div className="container d-flex ">
+                <input type="text" onChange={(e) => setsearch(e.target.value)} />
+                <button className="btn btn-primary ms-2" onClick={() => {
+                    setuser(users.filter((user) => user.name.toLowerCase() == search.toLowerCase()))
+                }}>Search</button>
+                <button className="btn btn-primary ms-2" onClick={() => {
+                    setuser(JSON.parse(localStorage.getItem("users")))
+                    setsearch("")
+                }}>Clear</button>
+                <button className="btn btn-warning text-white ms-2" onClick={() => {
+                    const arr = [...users]
+                    arr.sort((a, b) => a.Fees - b.Fees)
+                    setuser(arr);
+                }}>Low To High</button>
+                <button className="btn btn-warning text-white ms-2" onClick={() => {
+                    const arr = [...users]
+                    arr.sort((a, b) => b.Fees - a.Fees)
+                    setuser(arr);
+                }}>High To Low</button>
+            </div>
+            <div className="container d-flex  justify-content-center gap-4 mt-4" style={{ width: "20%", height: "auto" }}>
+                {
+                    users.map((use,i) => (
+                        <div className="shadow p-4 rounded-4 col">
+                            <div className="">
+                                <h3>{use.name}</h3>
+                                <span className="d-flex flex-column">{use.email}</span>
+                                <span className="d-flex flex-column">{use.phone}</span>
+                                <span className="d-flex flex-column">Fees : {use.Fees}</span>
+                                <button className="btn btn-danger" onClick={() => {
+                                    const array = [...users];
+                                    array.splice(i,1);
+                                    localStorage.setItem("users",JSON.stringify(array))
+                                    setuser(array);
+                                }}>Remove</button>
+                            </div>
+                        </div>
+                    ))
+                }
+            </div>
+        </>
+    )
 }
 export default User;
