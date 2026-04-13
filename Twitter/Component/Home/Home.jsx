0e9a50c3a08c77d83b964157d304fc70a88e04f1
DarshanPawar21 <dp7878038@gmail.com>
2026-04-13 16:56:36 +0530

    ....

---
+++ Twitter/Component/Home/Home.jsx
@@ -0,0 +1,10 @@
+const Home = () =>{
+    return(<>
+        <div className="container-fluid bg-primary" style={{width:"100%",height:"100vh"}}>
+            <div className="d-flex justify-content-center align-items-center" style={{height:"100%"}}>
+            <img className="" style={{width:"40%",height:"70%"}} src="https://media.tenor.com/y4gYHSUYKUYAAAAM/trump-maga.gif" alt="" />
+            </div>
+        </div>
+    </>)
+}
+export default Home;
--- Twitter/src/App.jsx
+++ Twitter/src/App.jsx
@@ -1,12 +1,14 @@
 // import { router } from "json-server";
 import SignIn from "../component/SignIn/SignIn";
 import SignUp from "../component/SignUp/SignUp";
+import Home from "../component/Home/Home"
 import { Routes,Route } from "react-router";
 const App = () =>{
   return(
     <Routes>
       <Route path="/" element={<SignUp />} />
       <Route path="/signin" element={<SignIn />} />
+      <Route path="/home" element={<Home />} />
     </Routes>
   )
 }
